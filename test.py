11a605252b19e227700f8afb21c891344900a1ba
hellothisjanu <138442715+hellothisjanu@users.noreply.github.com>
2024-08-16 16:54:02 +0530

    test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import os
 import streamlit as st
 
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
 
 st.title("Tweet Generator - V 🐦")
 
@@ -12,16 +12,15 @@ topic = st.text_input("Topic")
 number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
 
 
-# Import ChatOpenAI module
-from langchain_openai import ChatOpenAI
+# Using Google Models (Gemini Pro)
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-# Initialize OpenAI's GPT 3.5 model
-gpt3_model = ChatOpenAI(model_name = "gpt-3.5-turbo-0125")
-
+# Initialize Google's Gemini model
+gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
 
 if st.button("Generate"):
     prompt = f"Give me {number} tweets on {topic}."
-    response = gpt3_model.invoke(prompt)
+    response = gemini_model.invoke(prompt)
     st.write(response.content)
 
 
